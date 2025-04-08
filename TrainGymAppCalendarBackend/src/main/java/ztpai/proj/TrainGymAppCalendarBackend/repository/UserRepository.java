b2ea369faa6d58e3fbec77aa85f27f950d085f17
Konrad Kaczówka <konrisyt@gmail.com>
2025-04-08 17:58:34 +0200

    Created securityConfig, added method to UserRepo for login and register

--- TrainGymAppCalendarBackend/src/main/java/ztpai/proj/TrainGymAppCalendarBackend/repository/UserRepository.java
+++ TrainGymAppCalendarBackend/src/main/java/ztpai/proj/TrainGymAppCalendarBackend/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package ztpai.proj.TrainGymAppCalendarBackend.repository;
 
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ztpai.proj.TrainGymAppCalendarBackend.models.User;
 
@@ -9,4 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findUserById(Integer id);
+
+    Optional<User> findByMail(String mail);
 }
